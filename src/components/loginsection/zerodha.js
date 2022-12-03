48e82b7065efda1aa0ee81e792f95cc2a9c65dbb
trippingpanda <abhir009@gmail.com>
2022-12-03 11:05:35 +0100

    fiznr redirect

--- src/components/loginsection/zerodha.js
+++ src/components/loginsection/zerodha.js
@@ -18,7 +18,8 @@ class zerodha extends Component {
     render() {
         return (
             <section className="button-group">
-                <a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered d-none d-sm-inline-block">Login With Zerodha</a>
+                {/*<a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered d-none d-sm-inline-block">Login With Zerodha</a>*/}
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfMRXUhlbXYC7vhkF8PleiaIxRcMvoq9PpHdNath3cJBcPFRw/viewform" className="btn btn-bordered d-none d-sm-inline-block">Join Finzr</a>
                 <a href="#" className="btn btn-bordered d-none d-sm-inline-block">Read More</a>
             </section>
         );
