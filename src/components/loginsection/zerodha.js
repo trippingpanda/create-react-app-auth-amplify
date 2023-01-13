8b4752eeb15b8891be531b491101d782ce2ade0e
trippingpanda <abhir009@gmail.com>
2023-01-14 00:45:51 +0100

    fixing small screen issue

--- src/components/loginsection/zerodha.js
+++ src/components/loginsection/zerodha.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
-
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`;
 
 class zerodha extends Component {
-    state = {
-        data: {}
-    }
     render() {
         return (
             <section className="button-group">
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfMRXUhlbXYC7vhkF8PleiaIxRcMvoq9PpHdNath3cJBcPFRw/viewform" className="btn btn-bordered d-none d-sm-inline-block" target="_blank">Join Finzr</a>
-                <a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered d-none d-sm-inline-block">Check Zerodha Portfolio</a>
-                <a href="#" className="btn btn-bordered d-none d-sm-inline-block">Read More</a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfMRXUhlbXYC7vhkF8PleiaIxRcMvoq9PpHdNath3cJBcPFRw/viewform" className="btn btn-bordered" target="_blank">Join Finzr</a>
+                <a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered">Check Zerodha Portfolio</a>
+                <a href="#" className="btn btn-bordered">Read More</a>
             </section>
         );
     }
 }
+
 // var spans = document.getElementById('loginzerodha');
 // spans.onclick=doSomething;
 //
