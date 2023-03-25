5be55ed0cb206f8c7b40bcc5be49644286003a97
trippingpanda <abhir009@gmail.com>
2023-03-25 18:04:00 +0100

    remove zerodha link

--- src/components/loginsection/zerodha.js
+++ src/components/loginsection/zerodha.js
@@ -5,7 +5,7 @@ class zerodha extends Component {
         return (
             <section className="button-group">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSfMRXUhlbXYC7vhkF8PleiaIxRcMvoq9PpHdNath3cJBcPFRw/viewform" className="btn btn-bordered" target="_blank">Join Finzr</a>
-                <a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered">Check Zerodha Portfolio</a>
+                {/*<a href="https://kite.zerodha.com/connect/login?v=3&api_key=rzw4912554top6zz" className="btn btn-bordered">Check Zerodha Portfolio</a>*/}
                 {/*<a href="#" className="btn btn-bordered">Read More</a>*/}
             </section>
         );
