c73f362b4d11eb467a506ad1526a0966941f7c81
trippingpanda <abhir009@gmail.com>
2023-03-25 16:31:20 +0100

    remove auth

--- src/components/contactSection/contact.js
+++ src/components/contactSection/contact.js
@@ -6,7 +6,7 @@ const BASE_URL = "https://my-json-server.typicode.com/themeland/appo-json-2/them
 class contactSection extends Component {
     state = {
         data: { "heading": "Stay Tuned",
-            "headingText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati dignissimos quae quo ad iste ipsum officiis deleniti asperiores sit.",
+            "headingText": "Stay Tuned for More Features! We're constantly working to bring you the latest tools and resources to help you learn about the stock market and make informed investment decisions. Keep checking back for new features, courses, and tools that will help you take your investing skills to the next level. Don't miss out on our latest updates and offers – sign up for our newsletter and follow us on social media to stay in the loop!",
             "headingTextTwo": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati."}
     }
     componentDidMount(){
--- src/components/promoSection/promoOne.js
+++ src/components/promoSection/promoOne.js
@@ -10,25 +10,24 @@ class promoOne extends Component {
             "id": 1,
             "promoImage": "/img/promo_img_1.png",
             "promoTitle": "Learn From Scratch",
-            "promoText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, ipsum."
+            "promoText": "Our courses and resources are designed to help anyone, regardless of their level of experience, learn how to invest in the stock market from the ground up. Whether you're a complete beginner or have some experience, we can help you build a solid foundation in investing principles."
         },
             {
                 "id": 2,
                 "promoImage": "/img/promo_img_2.png",
                 "promoTitle": "Revision Classes",
-                "promoText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, ipsum."
+                "promoText": " Investing is an ongoing process that requires continuous learning and improvement. Our advanced courses and resources are designed to help you refine your investing skills and stay up-to-date with the latest market trends. Our \"Revision Classes\" are the perfect way to take your investing to the next level."
             },
             {
                 "id": 3,
                 "promoImage": "/img/promo_img_3.png",
                 "promoTitle": "Live Q&A Sessions",
-                "promoText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, ipsum."
-            },
+                "promoText": "We understand that investing can be complex and sometimes confusing. That's why we offer \"Live Q&A Sessions\" with experienced investors who can answer your questions and provide personalized guidance. Whether you're struggling with a specific investment or just looking for advice, our experts are here to help." },
             {
                 "id": 4,
                 "promoImage": "/img/promo_img_4.png",
                 "promoTitle": "Track Portfolio",
-                "promoText": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, ipsum."
+                "promoText": "Keeping track of your investments is essential to making informed decisions and maximizing your returns. Our platform provides tools and resources that will help you track your investments and make more informed decisions based on your investment goals. With our \"Track Portfolio\" feature, you'll always know how your investments are performing."
             }]
     }
     componentDidMount(){
